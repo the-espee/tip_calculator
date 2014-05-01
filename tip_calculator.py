c74032fb17baa713a0d099ab615d63edd26c5aa6
the-espee <steven.persaud@gmail.com>
2014-05-01 15:35:40 -0400

    final refactor #4 using optparse

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,16 +1,26 @@
-import sys
+from optparse import OptionParser
 
-meal = float(sys.argv[1])
-tax = float(sys.argv[2])
-tip = float(sys.argv[3])
+parser = OptionParser()
+parser.add_option("-m", "--meal", dest="meal", help="base cost of the meal",\
+				  type="float")
+parser.add_option("-x", "--tax", dest="tax_percent", type="float")
+parser.add_option("-t", "--tip", dest="tip_percent", help="tip % to leave",\
+				  type="float", default=.20)
 
-tax_value = meal * tax
-meal_with_tax = meal + meal * tax
-tip_value = meal_with_tax * tip
+(options, args) = parser.parse_args()
+
+if not (options.meal):
+	parser.error("Please indicate the base cost of your meal!")
+if not (options.tax_percent):
+	parser.error("Please indicate the tax percentage of your location!")
+
+tax_value = options.meal * options.tax_percent
+meal_with_tax = options.meal + options.meal * options.tax_percent
+tip_value = meal_with_tax * options.tip_percent
 total = meal_with_tax + tip_value
 
-print "The base cost of your meal is ${0:.2f}.".format(meal)
-print "The tax on your meal is ${0:.2f}.".format(tax)
+print "The base cost of your meal is ${0:.2f}.".format(options.meal)
+print "The tax on your meal is ${0:.2f}.".format(tax_value)
 print "The tip you should leave is ${0:.2f}.".format(tip_value)
 
 print "The total cost of your meal is ${0:.2f}.".format(total)
